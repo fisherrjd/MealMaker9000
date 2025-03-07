7c375bd5e4a742a39f451d3715e197935d450da8
jade fisher <fisherrjd@gmail.com>
2025-03-07 16:34:47 -0700

    begin definition

--- api.py
+++ api.py
@@ -1,3 +1,20 @@
 # TODO:
 # Outline goals
 # https://spoonacular.com/food-api/docs
+
+# config with defined macros:
+# Calories:
+
+# Protein:
+# Fats:
+# Carbs:
+
+# 3 Meals 
+# BUT breakfast is constant:
+# 2 eggs
+# 1 piece of sourdough
+# 1 tbsp of Blackberry Jam
+
+# Lunch
+# Dinner
+
